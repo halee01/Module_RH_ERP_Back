14740b013dcb246e0c5b0b5b55bcbc77f4100aad
sarra <sarramadeh@gmail.com>
2023-04-12 10:15:12 +0100

    language

--- src/main/java/com/csidigital/rh/shared/enumeration/Language.java
+++ src/main/java/com/csidigital/rh/shared/enumeration/Language.java
@@ -1,5 +1,40 @@
 package com.csidigital.rh.shared.enumeration;
 
 public enum Language {
-    ENGLISH
+    ARABIC("Arabic"),
+    AZERBAIJANI("Azerbaijani"),
+    BENGALI("Bengali"),
+    CHINESE_CANTONESE("Cantonese"),
+    CHINESE_MANDARIN("Mandarin Chinese"),
+    ENGLISH("English"),
+    FRENCH("French"),
+    GERMAN("German"),
+    GUJARATI("Gujarati"),
+    HINDI("Hindi"),
+    INDONESIAN("Indonesian"),
+    ITALIAN("Italian"),
+    JAPANESE("Japanese"),
+    JAVANESE("Javanese"),
+    KANNADA("Kannada"),
+    KOREAN("Korean"),
+    MALAY("Malay"),
+    MARATHI("Marathi"),
+    PERSIAN("Persian"),
+    POLISH("Polish"),
+    PORTUGUESE("Portuguese"),
+    PUNJABI("Punjabi"),
+    RUSSIAN("Russian"),
+    SPANISH("Spanish"),
+    TAMIL("Tamil"),
+    TELUGU("Telugu"),
+    TURKISH("Turkish"),
+    URDU("Urdu"),
+    VIETNAMESE("Vietnamese");
+    private  String language;
+    Language(String language) {
+        this.language = language;
+    }
+    public String getLanguage() {
+        return language;
+    }
 }
