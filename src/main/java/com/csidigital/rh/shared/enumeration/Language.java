482b79d0734be0959bea8da39d04dcf5aa4a2703
halee01 <halee.bk01@gmail.com>
2023-05-04 14:43:01 +0100

    5-4 first

--- src/main/java/com/csidigital/rh/shared/enumeration/Language.java
+++ src/main/java/com/csidigital/rh/shared/enumeration/Language.java
@@ -1,40 +1,33 @@
 package com.csidigital.rh.shared.enumeration;
 
 public enum Language {
-    ARABIC("Arabic"),
-    AZERBAIJANI("Azerbaijani"),
-    BENGALI("Bengali"),
-    CHINESE_CANTONESE("Cantonese"),
-    CHINESE_MANDARIN("Mandarin Chinese"),
-    ENGLISH("English"),
-    FRENCH("French"),
-    GERMAN("German"),
-    GUJARATI("Gujarati"),
-    HINDI("Hindi"),
-    INDONESIAN("Indonesian"),
-    ITALIAN("Italian"),
-    JAPANESE("Japanese"),
-    JAVANESE("Javanese"),
-    KANNADA("Kannada"),
-    KOREAN("Korean"),
-    MALAY("Malay"),
-    MARATHI("Marathi"),
-    PERSIAN("Persian"),
-    POLISH("Polish"),
-    PORTUGUESE("Portuguese"),
-    PUNJABI("Punjabi"),
-    RUSSIAN("Russian"),
-    SPANISH("Spanish"),
-    TAMIL("Tamil"),
-    TELUGU("Telugu"),
-    TURKISH("Turkish"),
-    URDU("Urdu"),
-    VIETNAMESE("Vietnamese");
-    private  String language;
-    Language(String language) {
-        this.language = language;
-    }
-    public String getLanguage() {
-        return language;
-    }
+    Arabe,
+    Azerbaïdjanais,
+    Bengali,
+    Cantonais,
+    Chinois_mandarin,
+    Anglais,
+    Français,
+    Allemand,
+    Gujarati,
+    Hindi,
+    iIndonésien,
+    Italien,
+    Japonais,
+    Javanais,
+    Kannada,
+    Coréen,
+    Malais,
+    Marathi,
+    Persan,
+    Polonais,
+    Portugais,
+    Pendjabi,
+    Russe,
+    Espagnol,
+    Tamoul,
+    Télougou,
+    Turc,
+    Ourdou,
+    Vietnamien
 }
