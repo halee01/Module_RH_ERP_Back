a66bd2dda0580bd4dd9845ffff58eeae91b404c5
sarra <sarramadeh@gmail.com>
2023-05-26 08:39:37 +0100

    26/05 8h39

--- src/main/java/com/csidigital/rh/management/service/impl/InterviewImpl.java
+++ src/main/java/com/csidigital/rh/management/service/impl/InterviewImpl.java
@@ -48,7 +48,7 @@ public class InterviewImpl implements InterviewService {
         Interview interviewSaved = interviewRepository.save(interview);
 
         // Add questions to the interview based on the question types and categories
-
+        if (request.getQuestionTypeIds() != null){
         for (QuestionType questionType : questionTypes) {
             List<QuestionCategory> questionCategory = questionType.getQuestionCategories();
             List<Question> questions = new ArrayList<>();
@@ -66,7 +66,7 @@ public class InterviewImpl implements InterviewService {
                 interviewSaved.getUpdatedQuestions().add(updatedQuestion);
             }
 
-        }
+        }}
 
         interviewRepository.save(interviewSaved);
 
